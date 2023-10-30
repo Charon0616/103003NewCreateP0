d0c8881768af00e934a2f6fb9e08580aba44a198
Davi Ji <v-weji1@microsoft.com>
2023-10-30 02:09:27 +0000

    make some changes and push it to the remote

--- ConsoleApp103003NewCreateP0/Program.cs
+++ ConsoleApp103003NewCreateP0/Program.cs
@@ -1,3 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 //make some changes and push it to the remote
+//make some changes and push it to the remote again
